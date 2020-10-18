a984f8d8b1a467ce54819ab40be68f1e0a9c226e
hadjiev0111 <amirhadjiev18@gmail.com>
2020-10-18 20:19:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,27 @@
+package com.example.taskapp.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.taskapp.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
